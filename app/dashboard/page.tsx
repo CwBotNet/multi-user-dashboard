e8785a5d71c5f3d610ed7b1e5a9ed30d46f7a5cf
Raj sahani <Rajsahaniofficial19@gmail.com>
2024-03-13 22:56:59 +0530

    initial commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+
+const DashboardPage = () => {
+    return (
+        <div className="flex justify-center items-center h-screen w-screen">
+            Dashboard Page
+        </div>
+    )
+}
+
+export default DashboardPage;
